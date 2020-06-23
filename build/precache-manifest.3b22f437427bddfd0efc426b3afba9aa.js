8fe99eae90c98fa407bc4315f24c10ba445d8bc2
Michaela Karkia <michaela@karkia.me>
2020-06-23 11:54:46 +0300

    phonebook fullstack 3.11 completed

---
+++ build/precache-manifest.3b22f437427bddfd0efc426b3afba9aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a19a13a44c6963e385821a5f22f0641",
+    "url": "/index.html"
+  },
+  {
+    "revision": "996d7180046d5e2f3362",
+    "url": "/static/css/main.d3559e4f.chunk.css"
+  },
+  {
+    "revision": "a75cfe734ddb11b2e053",
+    "url": "/static/js/2.2381a9e8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2381a9e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "996d7180046d5e2f3362",
+    "url": "/static/js/main.da727c1e.chunk.js"
+  },
+  {
+    "revision": "37e42bc47805ba865915",
+    "url": "/static/js/runtime-main.fac6ad05.js"
+  }
+]);
