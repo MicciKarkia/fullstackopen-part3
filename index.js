09906ae066ffc72a63f23d9c3615fe77e161d172
Michaela Karkia <michaela@karkia.me>
2020-06-29 12:17:16 +0300

    phonebook database step3 3.15 completed

--- index.js
+++ index.js
@@ -15,29 +15,22 @@ app.get('/api/persons', (request, response) => {
   })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  /*const id = Number(request.params.id)
-  console.log('id:', id)
-  const person = persons.find((person) => person.id === id)
-  console.log('person: ', person)
-
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }*/
-
-  Person.findById(request.params.id).then((person) => {
-    response.json(person)
-  })
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch((error) => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter((person) => person.id !== id)
-
-  response.status(204).end()
-})
+/*app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  
+})*/
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
@@ -54,6 +47,25 @@ app.post('/api/persons', (request, response) => {
     response.json(savedPerson)
   })
 })
+
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end()
+    })
+    .catch((error) => next(error))
+})
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
